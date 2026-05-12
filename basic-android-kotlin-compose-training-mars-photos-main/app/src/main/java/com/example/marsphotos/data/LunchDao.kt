1d7cd1e2c9cf8806bc324844f55050e9094e8845
Yamamichi <yamamichi.kt9@jsdata.co.jp>
2026-05-12 09:26:03 +0900

    no message

--- basic-android-kotlin-compose-training-mars-photos-main/app/src/main/java/com/example/marsphotos/data/LunchDao.kt
+++ basic-android-kotlin-compose-training-mars-photos-main/app/src/main/java/com/example/marsphotos/data/LunchDao.kt
@@ -1,14 +1,11 @@
 package com.example.marsphotos.data
 
-// この下の import たちが重要です！
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import androidx.room.Delete
 import kotlinx.coroutines.flow.Flow
-// LunchEntityが赤文字になる場合は、プロジェクト内での正しい場所をインポートしてください
-// 例: import com.example.marsphotos.data.LunchEntity
 
 @Dao
 interface LunchDao {
